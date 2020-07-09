d08e5fc2954d9ddf7505ea728055f49a483f30ac
mischkiey <mischkieycf@gmail.com>
2020-07-09 01:20:58 -0400

    Added cors and helmet

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const movies = require('./movie-data');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
 
 app.use(validateBearerToken = (req, res, next) => {
     const bearerToken = req.get('Authorization')
