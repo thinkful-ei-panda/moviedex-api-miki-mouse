a2467822a4d1746194349882312d6e42297f5fd2
mischkiey <mischkieycf@gmail.com>
2020-07-10 22:53:01 -0400

    Completed 1-for-all middleware logic

--- app.js
+++ app.js
@@ -25,71 +25,81 @@ app.use(validateBearerToken = (req, res, next) => {
     }
 )
 
-// Middleware for handling genre, country, avg_vote endpoints
+// Simple middleware for handling genre, country, avg_vote endpoints
 
 // const handleGetBySearchOption = (req, res) => {
-//     let searchResults = movies;
+//     let results = movies;
 
 //     if (req.query.genre) {
-//         searchResults = movies.filter(movie => movie[genre].toLowerCase().includes(req.query.genre.toLowerCase()));
+//         results = results.filter(result => result[genre].toLowerCase().includes(req.query.genre.toLowerCase()));
 //     }
 
 //     if (req.query.country) {
-//         searchResults = movies.filter(movie => movie[country].toLowerCase().includes(req.query.country.toLowerCase()));
+//         results = results.filter(result => result[country].toLowerCase().includes(req.query.country.toLowerCase()));
 //     }
 
 //     if (req.query.avg_vote) {
-//         searchResults = movies.filter(movie => Number(movie.avg_vote) >= Number(req.query.avg_vote));
+//         results = results.filter(result => Number(result.avg_vote) >= Number(req.query.avg_vote));
 //     }
 
-//     res
-//         .status(200)
-//         .json(searchResults)
-
+//     res.json(results)
 // }
-
-
 
 // 1-for-all search options middleware, for use if adding more search options and created for the extra fun
 // If adding more search options, inject into options array
 
+// const handleGetBySearchOption = (req, res) => {
+
+//     const options = ["genre", "country", "avg_vote"];
+//     const searchOption = Object.keys(req.query)[0];
+//     const searchKeyword = req.query[searchOption];
+
+//     if (!searchOption) {
+//         return res.json(movies)
+//     };
+
+//     if (!options.includes(searchOption)) {
+//         return res.send('Bad Request')
+//     };
+
+//     // Would it be better for this endpoint to return a 400 status?
+//     if (!searchKeyword) {
+//         return res.json(movies)
+//     };
+
+//     if (!isNaN(Number(searchKeyword))) {
+//         const searchResults = movies.filter(movie => movie[searchOption] >= (Number(searchKeyword)))
+//         return res.json(searchResults)  
+//     };
+
+//     const searchResults = movies.filter(movie => movie[searchOption].toLowerCase().includes(searchKeyword.toLowerCase()))
+//         res.json(searchResults) 
+// };
+
+// Improved 1-for-all middleware
+// Complete implementation of Mike Stowe's pseudocode
+
 const handleGetBySearchOption = (req, res) => {
+    let results = movies;
+    const options = Object.keys(movies[0]);
+    
+    if(Object.keys(req.query).length === 0) {
+        return res.json(movies)
+    }
 
-    const options = ["genre", "country", "avg_vote"];
-    const searchOption = Object.keys(req.query)[0];
-    const searchKeyword = req.query[searchOption];
+    for (let key in req.query) {
+        if (options.includes(key)) {
+            if (!isNaN(Number(req.query[key]))) {
+                results = results.filter(result => result[key] >= Number(req.query[key]));
+            } else {
+                results = results.filter(results => results[key].toLowerCase() === req.query[key].toLowerCase())
+            }
+        } else {
+            return res.status(400).send('Invalid request')
+        }
+    }
 
-    if (!searchOption) {
-        return res
-            .status(200)
-            .json(movies)
-    };
-
-    if (!options.includes(searchOption)) {
-        return res
-            .status(400)
-            .send('Bad Request')
-    };
-
-    // Would it be better for this endpoint to return a 400 status?
-    if (!searchKeyword) {
-        return res
-            .status(200)
-            .json(movies)
-    };
-
-    if (!isNaN(Number(searchKeyword))) {
-        const searchResults = movies.filter(movie => movie[searchOption] >= (Number(searchKeyword)))
-        return res
-            .status(200)
-            .json(searchResults)  
-    };
-
-    const searchResults = movies.filter(movie => movie[searchOption].toLowerCase().includes(searchKeyword.toLowerCase()))
-        res
-            .status(200)
-            .json(searchResults) 
-
+    return res.json(results)
 };
 
 app.get('/movie', handleGetBySearchOption);
