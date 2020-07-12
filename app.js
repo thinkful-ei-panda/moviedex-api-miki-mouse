e874b3ec378ae54074b00c03f4c532079e69722c
mischkiey <mischkieycf@gmail.com>
2020-07-12 17:34:47 -0400

    Code cleaned and setup for production

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const helmet = require('helmet');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny': 'common'
+
+app.use(morgan(morganSetting));
 app.use(cors());
 app.use(helmet());
 
@@ -85,6 +87,16 @@ const handleGetBySearchOption = (req, res) => {
 
 app.get('/movie', handleGetBySearchOption);
 
-app.listen(8000, () => {
-    console.log(`Server is meowing on PORT 8000`);
+app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }};
+    } else {
+      response = { error };
+    };
+    res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
