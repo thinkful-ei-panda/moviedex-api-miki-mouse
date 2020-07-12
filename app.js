1bc3d40e0eeb1f3f26bb7ece6ba2b4b6a3adb906
mischkiey <mischkieycf@gmail.com>
2020-07-12 11:03:52 -0400

    Added custom response for empty results array

--- app.js
+++ app.js
@@ -12,38 +12,15 @@ app.use(cors());
 app.use(helmet());
 
 app.use(validateBearerToken = (req, res, next) => {
-    const bearerToken = req.get('Authorization')
-    const apiToken = process.env.API_TOKEN
+    const bearerToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
     
     if (!bearerToken || bearerToken.split(' ')[1] !== apiToken) {
-        return res
-            .status(401)
-            .send('Unauthorized')
+        return res.status(401).send('Unauthorized');
     };
 
     next();
-    }
-)
-
-// Simple middleware for handling genre, country, avg_vote endpoints
-
-// const handleGetBySearchOption = (req, res) => {
-//     let results = movies;
-
-//     if (req.query.genre) {
-//         results = results.filter(result => result[genre].toLowerCase().includes(req.query.genre.toLowerCase()));
-//     }
-
-//     if (req.query.country) {
-//         results = results.filter(result => result[country].toLowerCase().includes(req.query.country.toLowerCase()));
-//     }
-
-//     if (req.query.avg_vote) {
-//         results = results.filter(result => Number(result.avg_vote) >= Number(req.query.avg_vote));
-//     }
-
-//     res.json(results)
-// }
+});
 
 // 1-for-all search options middleware, for use if adding more search options and created for the extra fun
 // If adding more search options, inject into options array
@@ -84,26 +61,30 @@ const handleGetBySearchOption = (req, res) => {
     const options = Object.keys(movies[0]);
     
     if(Object.keys(req.query).length === 0) {
-        return res.json(movies)
-    }
+        return res.json(movies);
+    };
 
     for (let key in req.query) {
         if (options.includes(key)) {
             if (!isNaN(Number(req.query[key]))) {
                 results = results.filter(result => result[key] >= Number(req.query[key]));
             } else {
-                results = results.filter(results => results[key].toLowerCase() === req.query[key].toLowerCase())
+                results = results.filter(results => results[key].toLowerCase() === req.query[key].toLowerCase());
             }
         } else {
-            return res.status(400).send('Invalid request')
+            return res.status(400).send('Invalid request');
         }
-    }
+    };
 
-    return res.json(results)
+    if (results.length === 0) {
+        return res.status(200).send('Unable to find movies that matched queries');
+    };
+
+    return res.json(results);
 };
 
 app.get('/movie', handleGetBySearchOption);
 
 app.listen(8000, () => {
-    console.log(`Server is meowing on PORT 8000`)
+    console.log(`Server is meowing on PORT 8000`);
 });
